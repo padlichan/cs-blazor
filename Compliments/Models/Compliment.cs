7e212cd147689a4b415963b1a94d371c869094de
Lili <lili.berenyi@gmail.com>
2025-01-21 12:28:27 +0000

    Add trees component

---
+++ Compliments/Models/Compliment.cs
@@ -0,0 +1,8 @@
+﻿namespace Compliments.Models
+{
+    public class Compliment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Compliments/Models/Tree.cs
@@ -0,0 +1,24 @@
+﻿namespace Compliments.Models
+{
+    public class Tree
+    {
+        private static int nextId = 1;
+        public Tree(string name, string location, int size, string species)
+        {
+            Id = nextId;
+            nextId++;
+            Name = name;
+            Location = location;
+            Size = size;
+            Species = species;
+            Compliments = [];
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Size { get; set; }
+        public string Species { get; set; }
+        public List<Compliment> Compliments { get; set; }
+    }
+}
